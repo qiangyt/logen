d2058544aeabdc832d5c1fd14a7c90a4a9d46ec5
Yiting Qiang <qiangyt@wxcount.com>
2022-05-29 16:35:41 +0000

    na

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeMap;
+
+fn main() -> Result<(), serde_yaml::Error> {
+    // You have some type.
+    let mut map = BTreeMap::new();
+    map.insert("x".to_string(), 1.0);
+    map.insert("y".to_string(), 2.0);
+
+    // Serialize it to a YAML string.
+    let s = serde_yaml::to_string(&map)?;
+    print!("{}\n", s);
+
+    // Deserialize it back to a Rust type.
+    let deserialized_map: BTreeMap<String, f64> = serde_yaml::from_str(&s)?;
+    //debug!("{}\n", deserialized_map);
+    Ok(())
 }
