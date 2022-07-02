63fb3aa898be05d06e04ba129a4ff8011e61ddc7
Yiting Qiang <qiangyt@wxcount.com>
2022-07-02 14:27:52 +0000

    na

--- src/bin/logen.rs
+++ src/bin/logen.rs
@@ -1,5 +1,5 @@
-use logen::app::{App};
-use handlebars::{Handlebars};
+use handlebars::Handlebars;
+use logen::app::App;
 use std::fs;
 
 fn main() {
@@ -10,31 +10,78 @@ fn main() {
 
     let mut handlebars = Handlebars::new();
     handlebars.register_escape_fn(|s| s.to_string());
-    handlebars.register_helper("align_left", Box::new(handlebars_helper__align_left));
-
+    handlebars.register_helper("align_left", Box::new(handlebars_helper_align_left));
+    handlebars.register_helper("align_right", Box::new(handlebars_helper_align_right));
 
     let mut app = App::new(&app_def, &mut handlebars);
     app.generate(&handlebars);
 }
 
-
-fn handlebars_helper__align_left(
+fn handlebars_helper_align_left(
     h: &handlebars::Helper,
     _: &Handlebars,
     _: &handlebars::Context,
     _: &mut handlebars::RenderContext,
     out: &mut dyn handlebars::Output,
 ) -> Result<(), handlebars::RenderError> {
-
     let value = h.param(0)
-        .ok_or(handlebars::RenderError::new("align_left(): invalid parameter 0 'value'"))?;
-    
+        .and_then(|v| v.value().as_str())
+        .ok_or(handlebars::RenderError::new(
+        "align_left(): invalid parameter 0 'value'",
+        ))?;
+
     let width = h
         .param(1)
         .and_then(|v| v.value().as_u64())
-        .ok_or(handlebars::RenderError::new("align_left(): invalid parameter 1 'width'"))? as u64;
-    
-    let rendered = format!("{} xx", value.render());
+        .ok_or(handlebars::RenderError::new(
+            "align_left(): invalid parameter 1 'width'",
+        ))? as u64;
+
+    let mut rendered = String::from(value);
+    let mut len = 0;
+    for _ in value.chars() {
+        len = len + 1;
+    }
+    while len < width {
+        rendered.push(' ');
+        len = len + 1;
+    }
+
+    out.write(rendered.as_ref())?;
+
+    Ok(())
+}
+
+fn handlebars_helper_align_right(
+    h: &handlebars::Helper,
+    _: &Handlebars,
+    _: &handlebars::Context,
+    _: &mut handlebars::RenderContext,
+    out: &mut dyn handlebars::Output,
+) -> Result<(), handlebars::RenderError> {
+    let value = h.param(0)
+        .and_then(|v| v.value().as_str())
+        .ok_or(handlebars::RenderError::new(
+        "align_right(): invalid parameter 0 'value'",
+        ))?;
+
+    let width = h
+        .param(1)
+        .and_then(|v| v.value().as_u64())
+        .ok_or(handlebars::RenderError::new(
+            "align_right(): invalid parameter 1 'width'",
+        ))? as u64;
+
+    let mut rendered = String::from(value);
+    let mut len = 0;
+    for _ in value.chars() {
+        len = len + 1;
+    }
+    while len < width {
+        rendered.insert(0, ' ');
+        len = len + 1;
+    }
+
     out.write(rendered.as_ref())?;
 
     Ok(())
