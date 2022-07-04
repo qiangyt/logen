dabad6043ddb0317614bfafce9574c2f82aee2af
Yiting Qiang <qiangyt@wxcount.com>
2022-07-04 06:28:35 +0000

    na

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs;
 fn main() {
     let config_yaml = fs::read_to_string("logen.default.yaml")
         .expect("failed to open config file: logen.default.yaml");
+
     let def: AppDef = serde_yaml::from_str(&config_yaml)
         .expect(&format!("failed to parse config yaml: {}", config_yaml));
 
