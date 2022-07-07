6c753d4ad167b8dad24d3109e5858ed72f525459
Yiting Qiang <qiangyt@wxcount.com>
2022-07-07 03:03:35 +0000

    na

--- src/config.rs
+++ src/config.rs
@@ -6,12 +6,16 @@ use anyhow::Result;
 use std::fs;
 use std::str;
 
+
+static ASSET_EXAMPLE_CONFIG_FILE: &str = "logen.config.example.yaml";
+static ARG_EXAMPLE_CONFIG_FILE: &str = "<example>";
+
 /// Log generator
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
 pub struct CliArgs {
     /// Path of the configuration file
-    #[clap(value_parser, required = false, default_value = "<example>")]
+    #[clap(value_parser, required = false, default_value = ARG_EXAMPLE_CONFIG_FILE)]
     pub config_file: String,
 
     /// Generates using the example configuration file
@@ -19,22 +23,21 @@ pub struct CliArgs {
     pub example: bool,
 }
 
-static EXAMPLE_CONFIG_FILE: &str = "logen.config.example.yaml";
 
 
 pub fn with_cli_args() -> Result<Option<AppDef>> {
     let args = CliArgs::parse();
 
     if args.example {
-        let a = Asset::get(EXAMPLE_CONFIG_FILE).unwrap();
+        let a = Asset::get(ASSET_EXAMPLE_CONFIG_FILE).unwrap();
         let yaml = String::from_utf8(a.data.as_ref().to_vec()).unwrap();
         return Ok(Some(AppDef::from_yaml(&yaml)))
     }
 
     let f = args.config_file;
-    if f == "<example>" {
+    if f == ARG_EXAMPLE_CONFIG_FILE {
         // output embedded example configuration
-        let a = Asset::get(EXAMPLE_CONFIG_FILE).unwrap();
+        let a = Asset::get(ASSET_EXAMPLE_CONFIG_FILE).unwrap();
         println!("{}", String::from_utf8(a.data.as_ref().to_vec())?);
         return Ok(None);
     }
